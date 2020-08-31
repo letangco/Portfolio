d297c684a2c0b7d5837d89fb1b63f6f37e40390d
TangCo <17520296@gm.uit.edu.vn>
2020-08-31 10:37:32 +0700

    ASdldn

--- index.js
+++ index.js
@@ -58,12 +58,12 @@ app.post("/api/forma", (req, res) => {
   });
 });
 
-if (process.env.NODE_ENV === "production"){
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production"){
+//   app.use(express.static("build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+//   });
+// }
 
 const PORT = process.env.PORT || 3001;
 
